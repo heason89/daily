2efd756d8c7d8378c321abea5d08ca5e7fac0413
DESKTOP-5AO8HO2\user <heason890325@gmail.com>
2025-04-25 14:14:36 +0800

    4/25

--- src/main/java/com/example/daily/controller/FoodServiceController.java
+++ src/main/java/com/example/daily/controller/FoodServiceController.java
@@ -1,5 +1,6 @@
 package com.example.daily.controller;
 
+import com.example.daily.vo.GetFoodRes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
