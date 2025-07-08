cc3ff71894f693b8e576f311b635b25e85f39de5
a7717516 <a7717516@gmail.com>
2025-07-08 14:58:09 +0800

    111

--- src/main/java/com/example/daily/dao/MealsDao.java
+++ src/main/java/com/example/daily/dao/MealsDao.java
@@ -36,13 +36,11 @@ public interface MealsDao extends JpaRepository<Meals, Integer> {
 	//當天
 	@Query(value = "select * from meals where user_id = ?1 and date(eat_time) = ?2", nativeQuery = true)
 	public List<Meals> GetDateMeals(int userId,LocalDate eatDate);
-<<<<<<< HEAD
-	
+
+
 	@Query(value = "select * from meals where user_id = ?1 and date(eat_time) = ?2 and meals_type = ?3", nativeQuery = true)
 	public Meals GetDateTypeMeals(int userId, LocalDate eatDate, String mealsType);
-=======
 
->>>>>>> 6c8e663b304761c950e86cde4ab21cfba6d675dc
 	@Modifying
 	@Transactional
 	@Query(value = "update meals set meals_name = :mealsName, eat_time = :eatTime, meals_type = :mealsType "
